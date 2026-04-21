bb1908230855f7c9d4c11a7e4d74657a4171b7ff
Aravindasamy v <aravindasamy.v.1001@gmail.com>
2026-04-21 17:34:07 +0530

    Add files via upload

---
+++ EMBEDDED_C/emc23.c
@@ -0,0 +1,42 @@
+#include<reg52.h>
+
+#define led P1
+
+unsigned int state=0; 
+unsigned int count=0;
+
+void timer(void) interrupt 3{
+TH1 = 0xDC;
+TL1 = 0x00;
+	count++;
+	if(count>=50){
+		if(state == 0){
+		led = 0x7E;
+			state = 1;
+		}
+		else{
+			led = 0xE7;
+			state = 0;
+		}
+		count = 0;
+	
+}
+	}
+
+void main(){
+	
+led = 0xE7;
+
+TMOD = 0x10;
+	
+TH1 = 0xDC;
+TL1 = 0x00;
+	
+EA  = 1;
+ET1 = 1;
+TR1 = 1;
+	
+	while(1){
+	}
+
+}
